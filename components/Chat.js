fe30d084ec7d6ed621ebf22169815f62bf2b43e4
tommasobenedetti <tommasobenedetti89@gmail.com>
2022-04-08 15:29:06 +0200

    5.3

--- components/Chat.js
+++ components/Chat.js
@@ -1,62 +1,126 @@
 import React from "react";
-import { View, Platform, KeyboardAvoidingView } from "react-native";
+import { View, Platform, KeyboardAvoidingView, StyleSheet } from "react-native";
 import { GiftedChat, Bubble } from 'react-native-gifted-chat'
+import * as firebase from "firebase";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDbGj4FltGaht04E20DuX3dxaZY3XdddX4",
+    authDomain: "chatapp-2d39f.firebaseapp.com",
+    projectId: "chatapp-2d39f",
+    storageBucket: "chatapp-2d39f.appspot.com",
+    messagingSenderId: "541513961857",
+};
 
 
 export default class Chat extends React.Component {
-    constructor() {
+    constructor(props) {
         super();
         this.state = {
-            message: [],
+            messages: [],
+            uid: 0,
+            user: {
+                _id: "",
+                name: "",
+                avatar: "",
+            },
         };
+
+        if (!firebase.apps.length) {
+            firebase.initializeApp(firebaseConfig);
+        }
+
+        this.referenceChatMessages = firebase.firestore().collection("messages");
     }
 
-    // Using component did mount to create the initial chat message and adding a bot image!
+    // Component did mount to create the initial chat message and adding a bot image!
+
     componentDidMount() {
-        let name = this.props.route.params.name;
+
+        let { name } = this.props.route.params;
+        // This will add the name at the top of the screen
         this.props.navigation.setOptions({ title: name });
 
-        this.setState({
-            messages: [
-                {
-                    _id: 1,
-                    text: 'Hello developer',
-                    createdAt: new Date(),
-                    user: {
-                        _id: 2,
-                        name: 'React Native',
-                        avatar: 'https://placeimg.com/140/140/any',
-                    },
+        this.authUnsubscribe = firebase.auth().onAuthStateChanged(async (user) => {
+            if (!user) {
+                await firebase.auth().signInAnonymously();
+            }
+
+            //update user state with currently active user data
+
+            this.setState({
+                uid: user.uid,
+                messages: [],
+                user: {
+                    _id: user.uid,
+                    name: name,
+                    avatar: "https://placeimg.com/140/140/any",
                 },
-                {
-                    _id: 2,
-                    text: `${name} has entered the chat`,
-                    createdAt: new Date(),
-                    system: true,
-                }
-            ],
+            });
+
+            this.unsubscribe = this.referenceChatMessages
+                .orderBy("createdAt", "desc")
+                .onSnapshot(this.onCollectionUpdate);
         });
     }
 
-    onSend(messages = []) {
-        this.setState((previousState) => ({
-            messages: GiftedChat.append(previousState.messages, messages),
-        }));
+    onCollectionUpdate = (querySnapshot) => {
+        const messages = [];
+        // go through each of the documents
+        querySnapshot.forEach((doc) => {
+            // get the QueryDocumentSnapshot's data
+            let data = doc.data();
+            messages.push({
+                _id: data._id,
+                text: data.text,
+                createdAt: data.createdAt.toDate(),
+                user: data.user,
+            });
+        });
+        this.setState({
+            messages: messages,
+        });
+    };
+
+    componentWillUnmount() {
+        //unsubscribe from collection updates
+        this.authUnsubscribe();
+        this.unsubscribe();
     }
 
-    // This will render the bubbles for the chat
-    renderBubble(props) {
-        return (
-            <Bubble
-                {...props}
-                wrapperStyle={{
-                    right: {
-                        backgroundColor: "#9d9d9d",
-                    },
-                }}
-            />
+    addMessages() {
+        const message = this.state.messages[0];
+        this.referenceChatMessages.add({
+            _id: message._id,
+            text: message.text || "",
+            createdAt: message.createdAt,
+            user: this.state.user,
+        });
+    }
+
+    getMessages = async () => {
+        let messages = "";
+        try {
+            messages = (await AsyncStorage.getItem("messages")) || [];
+            this.setState({
+                messages: JSON.parse(messages),
+            });
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
+
+    onSend(messages = []) {
+        this.setState(
+            (previousState) => ({
+                messages: GiftedChat.append(previousState.messages, messages),
+            }),
+            () => {
+                this.addMessages();
+            }
         );
     }
+
 
     // The view elements look like CSS but they are from React Native!
     render() {
@@ -68,11 +132,12 @@ export default class Chat extends React.Component {
                 backgroundColor: bgColor,
             }}>
                 <GiftedChat
-                    renderBubble={this.renderBubble.bind(this)}
                     messages={this.state.messages}
                     onSend={messages => this.onSend(messages)}
                     user={{
-                        _id: 1,
+                        _id: this.state.user._id,
+                        name: this.state.name,
+                        avatar: this.state.user.avatar,
                     }}
                 />
                 {Platform.OS === "android" ? (
@@ -82,3 +147,17 @@ export default class Chat extends React.Component {
         );
     }
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    giftedChat: {
+        flex: 1,
+        width: "75%",
+        paddingBottom: 10,
+        justifyContent: "center",
+        borderRadius: 5,
+    },
+});
